229036394b978abce73c0f1fc41bf5e6eb4cba2e
benbenl <evekana@gmail.com>
2016-05-03 16:33:11 -0700

    init

---
+++ verify.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Log In</title>
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta content="" name="description">
+<meta content="" name="author">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Loading Bootstrap -->
+<link href="dist/css/vendor/bootstrap.min.css" rel="stylesheet">
+
+<!-- Loading Flat UI -->
+<link href="dist/css/flat-ui.css" rel="stylesheet">
+
+<!-- Loading Customized UI -->
+
+<link rel="shortcut icon" href="img/favicon.png">
+<!-- Custom CSS -->
+<link href=
+    "http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
+    rel="stylesheet prefetch">
+<link href="css/style.css" rel="stylesheet">
+<style type="text/css">
+body {
+  padding: 10px 0px;
+  background-color: #f5f5f5;
+}
+</style>
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body class=" text-center">
+  <div class="container-fluid">
+  
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 col-sm-12">
+        <div class="login-form">
+        <header class="circle-logo knecta">
+			<span></span>
+		</header>
+          <form class="form-horizontal">
+            <div class="form-group">
+              <input type="password" class="form-control login-field" value="" placeholder="Enter new password" id="" />
+              <label class="login-field-icon fa fa-lock" for="login-name"></label>
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control login-field" value="" placeholder="Repeat new password" id="" />
+              <label class="login-field-icon fa fa-unlock-alt" for="login-pass"></label>
+            </div>
+            <div class="form-group">
+            
+              <label class="help-block"> By clicking "Save," I confirm that I accept the
+                <a href="http://enjoyney.com/terms/" target=
+                    "_blank">
+                  Terms of Service
+                </a>
+                and
+                <a href=
+                    "http://enjoyney.com/privacy-policy/" target=
+                    "_blank">
+                  Privacy Policy
+                </a>
+                . </label>
+            </div>
+            <div class="form-group">
+              <a class="btn btn-primary btn-lg btn-block btn-embossed btn-wide text-capitalize"  >
+                Save
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Bootstrap core JavaScript
+================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
